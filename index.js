13fbc14bdfded2c488dacf5400b0a5b7a84e30b9
Oleg Orlov <o.orlov@outlook.com>
2023-01-15 18:47:33 +0300

    Log search result with user input delay

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import MediaWiki from '/mediaWiki.js';
 
-const response = await MediaWiki.searchWikiPagesByTitle('javascript');
-console.log(response);
+const USER_INPUT_DELAY = 500;
+
+const searchEl = document.getElementById('search');
+
+let searchTimer;
+
+searchEl.oninput = () => {
+    clearTimeout(searchTimer);
+    searchTimer = setTimeout(
+        (title) => {
+            MediaWiki.searchWikiPagesByTitle(title)
+                .then((result) => console.log(result))
+                .catch((e) => console.log(e.message));
+        },
+        USER_INPUT_DELAY,
+        searchEl.value
+    );
+};
